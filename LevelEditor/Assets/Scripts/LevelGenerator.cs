b6a345f30a95a040208e9a9f05c8c7ef37f4d571
Khatim <kahtim.dar@gmail.com>
2018-04-27 23:33:39 +0400

    Added a Level Editor Script.

---
+++ LevelEditor/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D level;
+    // Use this for initialization
+    void Start()
+    {
+        LevelGenerate();
+    }
+
+    void LevelGenerate()
+    {
+        for (int i = 0; i < level.width; i++)
+        {
+            for (int j = 0; j < level.height; j++)
+            {
+                TileGenerate(i, j);
+            }
+        }
+    }
+
+    void TileGenerate(int x, int y)
+    {
+        Color pixel = level.GetPixel(x, y);
+		
+        if (pixel.a == 0)
+        {
+            return;
+        }
+    }
+}
