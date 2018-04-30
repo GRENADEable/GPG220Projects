a8e98786fca299e600d1e8fe0dfb4005ccf0e443
Khatim <kahtim.dar@gmail.com>
2018-04-30 13:54:30 +0400

    Initialized the scriptable objects scene

---
+++ LevelEditor/Assets/Scripts/Zombies.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombies : ScriptableObject
+{
+	public string zombieName;
+	public string zombieDescription;
+}
